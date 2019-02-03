826018b16d84a731199a3009a8cf9340994ba21e
scrudden <og.crudden@gmail.com>
2019-02-03 18:53:35 +0000

    More stuff to put GTFS-ride data into database. WIP.

--- transitclock/src/main/java/org/transitclock/db/structs/RiderTrip.java
+++ transitclock/src/main/java/org/transitclock/db/structs/RiderTrip.java
@@ -1,6 +1,8 @@
 package org.transitclock.db.structs;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.DynamicUpdate;
@@ -10,9 +12,146 @@ import org.transitclock.gtfsride.structs.GtfsRideRiderTrip;
 @DynamicUpdate
 @Table(name = "RiderTrip")
 public class RiderTrip {
-
-	public RiderTrip(GtfsRideRiderTrip trip) {
+	@Id	
+	@Column
+	String riderId;
+	@Id
+	@Column
+	String agencyId;
+	@Column
+	String tripId;	
+	@Id
+	@Column
+	String boardingStopId;
+	@Column
+	Integer boardingStopSequence;
+	@Column
+	String alightingStopId;
+	@Column
+	Integer alightingStopSequence;
+	@Id
+	@Column
+	String serviceDate;
+	@Id
+	@Column
+	String boardingTime;
+	@Column
+	String alightingTime;
+	@Column
+	Integer riderType;
+	@Column
+	String riderTypeDescription;
+	@Column
+	Double farePaid;
+	@Column
+	Integer transactionType;
+	@Column
+	Integer fareMedia;
+	@Column
+	Integer accompanyingDevice;
+	@Column
+	Boolean transferStatus;
+	public RiderTrip()
+	{
+		this.agencyId=null;
+		this.riderId=null;
+		this.tripId=null;
+		this.boardingStopId=null;;
+		this.boardingStopSequence=null;
+		this.alightingStopId=null;
+		this.alightingStopSequence=null;
+		this.serviceDate=null;
+		this.boardingTime=null;
+		this.alightingTime=null;
+		this.riderType=null;
+		this.riderTypeDescription=null;
+		this.farePaid=null;
+		this.transactionType=null;
+		this.fareMedia=null;
+		this.accompanyingDevice=null;
+		this.transferStatus=null;
 		
 	}
-
+	public RiderTrip(GtfsRideRiderTrip trip) {
+		this.agencyId=trip.getAgencyId();
+		this.riderId=trip.getRiderId();
+		this.tripId=trip.getTripId();
+		this.boardingStopId=trip.getBoardingStopId();
+		this.boardingStopSequence=trip.getBoardingStopSequence();
+		this.alightingStopId=trip.getAlightingStopId();
+		this.alightingStopSequence=trip.getAlightingStopSequence();
+		this.serviceDate=trip.getServiceDate();
+		this.boardingTime=trip.getBoardingTime();
+		this.alightingTime=trip.getAlightingTime();
+		this.riderType=trip.getRiderType();
+		this.riderTypeDescription=trip.getRiderTypeDescription();
+		this.farePaid=trip.getFarePaid();
+		this.transactionType=trip.getTransactionType();
+		this.fareMedia=trip.getFareMedia();
+		this.accompanyingDevice=trip.getAccompanyingDevice();
+		this.transferStatus=trip.getTransferStatus();
+		
+	}
+	public String getRiderId() {
+		return riderId;
+	}
+	public String getAgencyId() {
+		return agencyId;
+	}
+	public String getTripId() {
+		return tripId;
+	}
+	public String getBoardingStopId() {
+		return boardingStopId;
+	}
+	public Integer getBoardingStopSequence() {
+		return boardingStopSequence;
+	}
+	public String getAlightingStopId() {
+		return alightingStopId;
+	}
+	public Integer getAlightingStopSequence() {
+		return alightingStopSequence;
+	}
+	public String getServiceDate() {
+		return serviceDate;
+	}
+	public String getBoardingTime() {
+		return boardingTime;
+	}
+	public String getAlightingTime() {
+		return alightingTime;
+	}
+	public Integer getRiderType() {
+		return riderType;
+	}
+	public String getRiderTypeDescription() {
+		return riderTypeDescription;
+	}
+	public Double getFarePaid() {
+		return farePaid;
+	}
+	public Integer getTransactionType() {
+		return transactionType;
+	}
+	public Integer getFareMedia() {
+		return fareMedia;
+	}
+	public Integer getAccompanyingDevice() {
+		return accompanyingDevice;
+	}
+	public Boolean getTransferStatus() {
+		return transferStatus;
+	}
+	@Override
+	public String toString() {
+		return "RiderTrip [riderId=" + riderId + ", agencyId=" + agencyId + ", tripId=" + tripId + ", boardingStopId="
+				+ boardingStopId + ", boardingStopSequence=" + boardingStopSequence + ", alightingStopId="
+				+ alightingStopId + ", alightingStopSequence=" + alightingStopSequence + ", serviceDate=" + serviceDate
+				+ ", boardingTime=" + boardingTime + ", alightingTime=" + alightingTime + ", riderType=" + riderType
+				+ ", riderTypeDescription=" + riderTypeDescription + ", farePaid=" + farePaid + ", transactionType="
+				+ transactionType + ", fareMedia=" + fareMedia + ", accompanyingDevice=" + accompanyingDevice
+				+ ", transferStatus=" + transferStatus + "]";
+	}
+	
 }
--- transitclock/src/main/java/org/transitclock/db/structs/Ridership.java
+++ transitclock/src/main/java/org/transitclock/db/structs/Ridership.java
@@ -1,5 +1,6 @@
 package org.transitclock.db.structs;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,9 +11,152 @@ import org.transitclock.gtfsride.structs.GtfsRideRidership;
 @DynamicUpdate
 @Table(name = "Ridership")
 public class Ridership {
-
+	@Column
+	Integer totalBoardings;
+	@Column
+	Integer totalAlightings;
+	@Column
+	String ridershipStartDate;
+	@Column
+	String ridershipEndDate;
+	@Column
+	String ridershipStartTime;
+	@Column
+	String ridershipEndTime;
+	@Column
+	String serviceId;
+	@Column
+	Boolean monday;
+	@Column
+	Boolean tuesday;
+	@Column
+	Boolean wednesday;
+	@Column
+	Boolean thursday;
+	@Column
+	Boolean friday;
+	@Column
+	Boolean saturday;
+	@Column
+	Boolean sunday;
+	@Column
+	String agencyId;
+	@Column
+	String routeId;
+	@Column
+	String directionId;
+	@Column
+	String tripId;
+	@Column
+	String stopId;
+	public Ridership() {
+		totalBoardings=null;
+		totalAlightings=null;
+		ridershipStartDate=null;
+		ridershipEndDate=null;
+		ridershipStartTime=null;
+		ridershipEndTime=null;
+		serviceId=null;
+		monday=null;
+		tuesday=null;
+		wednesday=null;
+		thursday=null;
+		friday=null;
+		saturday=null;
+		sunday=null;
+		agencyId=null;
+		routeId=null;
+		directionId=null;
+		tripId=null;
+		stopId=null;
+	}
 	public Ridership(GtfsRideRidership ridership) {
-		// TODO Auto-generated constructor stub
+		totalBoardings=ridership.getTotalBoardings();
+		totalAlightings=ridership.getTotalAlightings();
+		ridershipStartDate=ridership.getRidershipStartDate();
+		ridershipEndDate=ridership.getRidershipEndDate();
+		ridershipStartTime=ridership.getRidershipStartTime();
+		ridershipEndTime=ridership.getRidershipEndTime();
+		serviceId=ridership.getServiceId();
+		monday=ridership.getMonday();
+		tuesday=ridership.getTuesday();
+		wednesday=ridership.getWednesday();
+		thursday=ridership.getThursday();
+		friday=ridership.getFriday();
+		saturday=ridership.getSaturday();
+		sunday=ridership.getSunday();
+		agencyId=ridership.getAgencyId();
+		routeId=ridership.getRouteId();
+		directionId=ridership.getDirectionId();
+		tripId=ridership.getTripId();
+		stopId=ridership.getStopId();
+	}
+	public Integer getTotalBoardings() {
+		return totalBoardings;
+	}
+	public Integer getTotalAlightings() {
+		return totalAlightings;
+	}
+	public String getRidershipStartDate() {
+		return ridershipStartDate;
+	}
+	public String getRidershipEndDate() {
+		return ridershipEndDate;
+	}
+	public String getRidershipStartTime() {
+		return ridershipStartTime;
+	}
+	public String getRidershipEndTime() {
+		return ridershipEndTime;
+	}
+	public String getServiceId() {
+		return serviceId;
+	}
+	public Boolean getMonday() {
+		return monday;
+	}
+	public Boolean getTuesday() {
+		return tuesday;
+	}
+	public Boolean getWednesday() {
+		return wednesday;
+	}
+	public Boolean getThursday() {
+		return thursday;
+	}
+	public Boolean getFriday() {
+		return friday;
+	}
+	public Boolean getSaturday() {
+		return saturday;
+	}
+	public Boolean getSunday() {
+		return sunday;
+	}
+	public String getAgencyId() {
+		return agencyId;
+	}
+	public String getRouteId() {
+		return routeId;
+	}
+	public String getDirectionId() {
+		return directionId;
+	}
+	public String getTripId() {
+		return tripId;
+	}
+	public String getStopId() {
+		return stopId;
+	}
+	@Override
+	public String toString() {
+		return "Ridership [totalBoardings=" + totalBoardings + ", totalAlightings=" + totalAlightings
+				+ ", ridershipStartDate=" + ridershipStartDate + ", ridershipEndDate=" + ridershipEndDate
+				+ ", ridershipStartTime=" + ridershipStartTime + ", ridershipEndTime=" + ridershipEndTime
+				+ ", serviceId=" + serviceId + ", monday=" + monday + ", tuesday=" + tuesday + ", wednesday="
+				+ wednesday + ", thursday=" + thursday + ", friday=" + friday + ", saturday=" + saturday + ", sunday="
+				+ sunday + ", agencyId=" + agencyId + ", routeId=" + routeId + ", directionId=" + directionId
+				+ ", tripId=" + tripId + ", stopId=" + stopId + "]";
 	}
 
 }
